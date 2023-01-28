0ccad6c78a4587adfdd76cd3cfa8bef88d07352f
deepredk <deepred.kim@gmail.com>
2023-01-28 10:50:57 +0900

    chore: 프로필 이미지 변경

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "deepred.blog",
   description: "Backend 취준생입니다.",
   author: "Deepred",
-  siteUrl: "https://deepred.blog/",
+  siteUrl: "https://deepred.blog",
   links: {
     github: "https://github.com/deepredk",
     email: "mailto:deepred.kim@gmail.com",
